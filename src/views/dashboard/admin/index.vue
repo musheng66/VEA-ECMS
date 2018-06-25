8d98c4949d4f504fa43810c0ed212ed88bd870d2
MuSheng <425893642@qq.com>
2018-06-25 14:30:46 +0800

    对登录时间进行判断

--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -2,8 +2,8 @@
   <div class="dashboard-editor-container">
     <section class="page">
       <article>
-        <h1>欢迎</h1>
-        <h2>{{ username }}</h2>
+        <h1>登录成功！</h1>
+        <h2>{{ timeStr }}，{{ name }}</h2>
         <h5>登录时间： {{ loginTime }}</h5>
       </article>
     </section>
@@ -12,6 +12,24 @@
 
 <script>
   import { mapGetters } from 'vuex'
+  import Moment from 'moment'
+
+  Moment.locale('zh-cn', {
+    meridiem : function (hour, minute, isLowercase) {
+      if (hour < 9) {
+        return "早上";
+      } else if (hour < 11 && minute < 30) {
+        return "上午";
+      } else if (hour < 13 && minute < 30) {
+        return "中午";
+      } else if (hour < 18) {
+        return "下午";
+      } else {
+        return "晚上";
+      }
+    }
+  });
+
   export default {
     name: 'dashboard-admin',
     components: {
@@ -19,16 +37,22 @@
     },
     data() {
       return {
-
+        timeStr: ''
       }
     },
+    created() {
+      this.initTimeStr()
+    },
     methods: {
-
+      initTimeStr() {
+        const time = Moment.localeData(this.loginTime);
+        this.timeStr = time.meridiem(Moment(this.loginTime).hour(), Moment(this.loginTime, false).minute()) + '好';
+      }
     },
     computed: {
       // 映射 getters
       ...mapGetters({
-        username: 'name',
+        name: 'name',
         loginTime: 'loginTime'
       })
     }
