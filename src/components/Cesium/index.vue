e4031f630a3a6ea3db439e625139511c9d1332da
chenran <18611652288@163.com>
2018-11-05 14:43:21 +0800

    修改Cesium的Token

--- src/components/Cesium/index.vue
+++ src/components/Cesium/index.vue
@@ -34,10 +34,36 @@
 
       // 初始化cesium
       initCesium() {
-        Cesium.BingMapsApi.defaultKey = 'AsarFiDvISunWhi137V7l5Bu80baB73npU98oTyjqKOb7NbrkiuBPZfDxgXTrGtQ'
+        // Cesium.BingMapsApi.defaultKey = 'AsarFiDvISunWhi137V7l5Bu80baB73npU98oTyjqKOb7NbrkiuBPZfDxgXTrGtQ'
+        Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxNTRkYzYyMi1jNWQ0LTQ5YTMtOWI2Yi1mYmVjZGMzNDZiMjQiLCJpZCI6NDY2NCwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU0MTM5NDcwMH0.krxIfU86Ni50sGk7aqobc2LBP09yKNEG5hqK94ae8yA'
         console.log('cesium start ...')
         let viewer = new Cesium.Viewer('cesiumContainer')
         console.log(viewer, 'cesium created ...')
+
+        let wyoming = viewer.entities.add({
+          name : 'Wyoming',
+          polygon : {
+            hierarchy : Cesium.Cartesian3.fromDegreesArray([
+              -109.080842,45.002073,
+              -105.91517,45.002073,
+              -104.058488,44.996596,
+              -104.053011,43.002989,
+              -104.053011,41.003906,
+              -105.728954,40.998429,
+              -107.919731,41.003906,
+              -109.04798,40.998429,
+              -111.047063,40.998429,
+              -111.047063,42.000709,
+              -111.047063,44.476286,
+              -111.05254,45.002073]),
+            height : 0,
+            material : Cesium.Color.RED.withAlpha(0.5),
+            outline : true,
+            outlineColor : Cesium.Color.BLACK
+          }
+        })
+        viewer.zoomTo(wyoming)
+
       }
     },
   }
